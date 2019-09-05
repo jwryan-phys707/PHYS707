2f4bf99a43ae314e771442a2ea3ea9ad3db28e41
jwryan-phys707 <54957907+jwryan-phys707@users.noreply.github.com>
2019-09-05 14:28:37 -0500

    Add files via upload

--- Joe_Triangles.py
+++ Joe_Triangles.py
@@ -4,9 +4,10 @@ Created on Thu Sep  5 11:46:20 2019
 
 @author: Joe Ryan
 """
+
 from numpy import arccos, random, sqrt, pi
 
-l = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 40] #Side lengths of box in d dimensions.
+l = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1] #Side lengths of box in d dimensions.
 d = len(l) #Number of dimensions.
 L = 10**5 #Number of trials.
 
@@ -26,9 +27,9 @@ for p in range(0, L, 1):
     b2 = []
     c2 = []
     for p in range(0, d, 1):
-        lqa = q[0][0][p] - q[1][0][p]
-        lqb = q[0][0][p] - q[2][0][p]
-        lqc = q[1][0][p] - q[2][0][p]
+        lqa = l[p]*(q[0][0][p] - q[1][0][p])
+        lqb = l[p]*(q[0][0][p] - q[2][0][p])
+        lqc = l[p]*(q[1][0][p] - q[2][0][p])
         
         a2.append(lqa**2)
         b2.append(lqb**2)
@@ -37,7 +38,7 @@ for p in range(0, L, 1):
     a = sqrt(sum(a2))
     b = sqrt(sum(b2))
     c = sqrt(sum(c2))
-    #Lines 60-74 implement the generalized form of the Pythagorean
+    #Lines 26-40 implement the generalized form of the Pythagorean
     #theorem: r_ij = [(q_i1 - q_j1)^2 + ... + (q_id - q_jd)^2]^(1/2)
     #for each side length a, b, c. The second index on q labels the
     #coordinate (e.g. x = 1, y = 2, z = 3 in 3 dimensions);
@@ -57,12 +58,12 @@ for p in range(0, L, 1):
         ob.append(ob1)
     
 #Number of acute triangles divided by number of trials = P(acute).
-#Number of obtuse triangles divided by number of trials = P(obtuse).
 print('P(acute) = ', len(ac)/L)
+#Number of obtuse triangles divided by number of trials = P(obtuse).
 print('P(obtuse) = ', len(ob)/L)
 
 if all(elem == l[0] for elem in l):
-    print(str(d) + ' dimensions with equal side lengths.')
+    print(str(d) + '-dimensional box with equal side lengths.')
 else:
-    print(str(d) + ' dimensions with unequal side lengths.')
+    print(str(d) + '-dimensional box with unequal side lengths.')
 
